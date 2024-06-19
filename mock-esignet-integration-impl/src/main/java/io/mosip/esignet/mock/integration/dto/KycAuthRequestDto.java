2b637f11b14c46301f0072c2b7f403bd5a0c3827
Venkata Saidurga Polamraju <saidurgacsea@gmail.com>
2024-06-19 16:00:37 +0530

    Added pulgins
    Signed-off-by: Venkata Saidurga Polamraju <saidurgacsea@gmail.com>

---
+++ mock-esignet-integration-impl/src/main/java/io/mosip/esignet/mock/integration/dto/KycAuthRequestDto.java
@@ -0,0 +1,18 @@
+package io.mosip.esignet.mock.integration.dto;
+
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class KycAuthRequestDto {
+
+    private String transactionId;
+    private String individualId;
+    private String otp;
+    private String pin;
+    private String biometrics;
+    private String kba;
+    private List<String> tokens;
+}
